a7b1d28e91359e6664d2babdd9796dbe7c747cd2
hikettei <hikettei@gmail.com>
2025-11-21 23:18:23 +0900

    Add SPECS.md, refactor OpType, fix loop fusion schedule, and restore polyhedral exports.

--- caten/polyhedral/__init__.py
+++ caten/polyhedral/__init__.py
@@ -1,7 +1,21 @@
+from .analysis import compute_flow
+from .codegen import to_c
 from .schedule import PolyhedralSchedule
+from .schedule_tree.band import band
+from .schedule_tree.domain import domain
+from .schedule_tree.filter import filter
+from .schedule_tree.mark import mark
+from .schedule_tree.sequence import sequence
 from .scop import Computation, Scop, build_scop
 
 __all__ = [
     "Scop", "Computation", "build_scop",
     "PolyhedralSchedule",
+    "domain",
+    "band",
+    "sequence",
+    "filter",
+    "mark",
+    "compute_flow",
+    "to_c",
 ]
