908979693ad1e7ac9ec451cc7b87a5711fd0e374
hikettei <hikettei@gmail.com>
2025-12-18 20:03:47 +0900

    concepts?

--- caten/polyhedral/schedule_tree.py
+++ caten/polyhedral/schedule_tree.py
@@ -114,9 +114,19 @@ class domain(ScheduleNodeBase):
 # Symbolic Tileどこ？
 # Building Mode vs After Build Mode
 # - 両方で，統一的に，ループ変換を行える抽象化は何？
+# Construction Phase => Optimization Phase
+# with (P.directive("gpu") | P.domain(...)) as f:
+#   f = (f @ [3, 3])
+#   同じStyleを2回繰り返して書く
+# with P.domain(...) as f: # BEAM Searchするから，こっちもTree, directive.py
+#   pass
+# Polyhedral Scalar Representation
+# Directive, Baseの二つのデータ構造はある
+# - or, transformationをlazy evalする
 # https://dl.acm.org/doi/epdf/10.1145/3178372.3179509
 # https://inria.hal.science/hal-02493164v2/document
 # 後もう一本読むべき論文があったはず・・・
+# Symbolic Tileを実現するには，Domain <-> Bandの間で，計算グラフを構築する必要がある。
 class band(ScheduleNodeBase):
     """
     TODO: Decent docs?
