e72883f986478d9a338b614a4033cafe46632d8e
hikettei <hikettei@gmail.com>
2025-12-25 22:18:40 +0900

    helpers

--- caten/helpers.py
+++ caten/helpers.py
@@ -2,19 +2,19 @@ from __future__ import annotations
 
 import functools
 import operator
-from typing import Iterable, TypeVar
+from typing import Iterable, TypeVar, Any
 
 T = TypeVar("T")
 
 def prod(x:Iterable[T]) -> T|int: return functools.reduce(operator.mul, x, 1)
 
-def argfix(*x):
+def argfix(*x: Any) -> tuple[Any, ...]:
   if x and x[0].__class__ in (tuple, list):
     if len(x) != 1: raise ValueError(f"bad arg {x}")
     return tuple(x[0])
   return x
 
-def align_left(*shapes):
+def align_left(*shapes: tuple[int, ...]) -> tuple[tuple[int, ...], ...]:
   # unsqueeze left to make every shape same length
   max_dim = max(len(shape) for shape in shapes)
   return tuple((1,) * (max_dim - len(shape)) + shape for shape in shapes)
